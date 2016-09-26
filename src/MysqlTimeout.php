77ec3b7889842719bc429af15736b9787ea38348
wangyang <357466524@qq.com>
2016-09-26 11:04:00 +0800

    0.1.0

--- src/MysqlTimeout.php
+++ src/MysqlTimeout.php
@@ -16,22 +16,16 @@ class MysqlTimeout
     public $timeout = 3;
     function __construct($config){
         $this->db = new mysqli($config['host'], $config['user'], $config['password'], $config['dbname'], $config['port']);
-        $this->timeout = $config['timeout'] ?: 3;
+        $this->timeout = isset($config['timeout']) ? $config['timeout'] : 3;
         $this->db->set_charset($config['charset']);
     }
 
-    /**
-     * @param mixed $sql 
-     * @param mixed $timeout 
-     * @return mixed 
-     */
-    public function query($sql,$timeout = null){
+    protected function getResult($sql,$timeout,$func){
         $timeout == null && $timeout = 3;
         $this->db->query($sql, MYSQLI_ASYNC);
         $all_links = array($this->db);
         $processed = 0;
         $begin = microtime(true);
-        $ret = null;
         do {
             $links = $errors = $reject = array();
             foreach ($all_links as $link) {
@@ -39,18 +33,16 @@ class MysqlTimeout
             }
             if (!mysqli_poll($links, $errors, $reject, 0, 50000)) {
                 if(microtime(true)-$begin > $timeout){
+                    $result = $this->db->reap_async_query();
+                    mysqli_free_result($result);
+                    throw new Exception('timeout',922922);
                     break;
                 }
                 continue;
             }
             foreach ($links as $link) {
                 if ($result = $link->reap_async_query()) {
-                    while($row = $result->fetch_assoc()){
-                        $ret[] = $row;
-                    }
-                    if (is_object($result)){
-                        mysqli_free_result($result);
-                    }
+                    is_callable($func) && $func($result,$link);
                 } else {
                     $this->error = sprintf("MySQLi Error: %s", mysqli_error($link));
                     throw new Exception($this->error);
@@ -58,44 +50,58 @@ class MysqlTimeout
                 $processed++;
             }
         } while ($processed < count($all_links));
+    }
+
+    public function query($sql,$timeout = null){
+        $ret = null;
+        $this->getResult($sql,$timeout,function($result) use (&$ret){
+            //while($row = $result->fetch_assoc()){
+            //    $ret[] = $row;
+            //}
+            $ret = $result->fetch_all(MYSQL_ASSOC);
+            if (is_object($result)){
+                mysqli_free_result($result);
+            }
+        });
         return $ret;
     }
 
-    public function execute($sql,$timeout = null){
-        $timeout == null && $timeout = 3;
-        $this->db->query($sql, MYSQLI_ASYNC);
-        $all_links = array($this->db);
-        $processed = 0;
-        $begin = microtime(true);
-        $ret = null;
-        do {
-            $links = $errors = $reject = array();
-            foreach ($all_links as $link) {
-                $links[] = $errors[] = $reject[] = $link;
+    public function update($sql,$timeout = null){
+        $ret = false;
+        $this->getResult($sql,$timeout,function($result,$link) use (&$ret){
+            if ($result === TRUE){
+                $this->affected_rows = $link->affected_rows;
+                $ret = $this->affected_rows;
+            }else{
+                $this->error = $link->error;
+                $this->errno = $link->errno;
             }
-            if (!mysqli_poll($links, $errors, $reject, 0, 50000)) {
-                if(microtime(true)-$begin > $timeout){
-                    break;
-                }
-                continue;
+            if (is_object($result)){
+                mysqli_free_result($result);
             }
-            foreach ($links as $link) {
-                if ($result = $link->reap_async_query()) {
-                    $ret = $result;
-                    if ($result === TRUE){
-                        $this->affected_rows = $link->affected_rows;
-                    }else{
-                        $this->error = $link->error;
-                        $this->errno = $link->errno;
-                    }
-                } else {
-                    $this->error = sprintf("MySQLi Error: %s", mysqli_error($link));
-                    throw new Exception($this->error);
-                };
-                $processed++;
-            }
-        } while ($processed < count($all_links));
+        });
         return $ret;
+    }
+
+    public function insert($sql,$timeout = null){
+        $ret = false;
+        $this->getResult($sql,$timeout,function($result,$link) use (&$ret){
+            if ($result === TRUE){
+                $this->insert_id = $link->insert_id;
+                $ret = $this->insert_id;
+            }else{
+                $this->error = $link->error;
+                $this->errno = $link->errno;
+            }
+            if (is_object($result)){
+                mysqli_free_result($result);
+            }
+        });
+        return $ret;
+    }
+
+    public function delete($sql,$timeout = null){
+        $this->update($sql,$timeout);
     }
 
     public function insert_id(){
